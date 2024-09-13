68d8206061f25bceecf4a07392a8c902724a3046
Denis-Frunza <frunzadenis.93@gmail.com>
2024-09-13 09:34:07 +0300

    add templates

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 import inspect
 from typing import Any
 import inspect
@@ -7,11 +8,15 @@ from parse import parse
 from webob import Request, Response
 from requests import Session as RequestsSession
 from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
+from jinja2 import Environment, FileSystemLoader
 
 
 class API:
-    def __init__(self) -> None:
+    def __init__(self, templates_dir="templates") -> None:
         self.routes = {}
+        self.templates_env = Environment(
+            loader=FileSystemLoader(os.path.abspath(templates_dir))
+        )
 
     def __call__(self, environ, start_response, *args: Any, **kwds: Any) -> Any:
         request = Request(environ)
@@ -63,6 +68,13 @@ class API:
     def default_response(self, response):
         response.status_code = 404
         response.text = "Not found."
+
+    def template(self, template_name, context=None):
+        if context is None:
+            context = {}
+
+        return self.templates_env.get_template(template_name).render(**context)
+
 
     def test_session(self, base_url="http://testserver"):
         session = RequestsSession()
